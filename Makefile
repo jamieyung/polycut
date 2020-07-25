cbe4a356404992a73a0e2ec939fbdc158ee15e82
Jamie Yung <jamie.yung286@gmail.com>
2020-07-25 15:19:21 +1000

    Added auto-cache busting and minification of assets to dist build process.

--- Makefile
+++ Makefile
@@ -9,12 +9,5 @@ sass:
 build:
 	spago bundle-app --to web/app.js
 
-# For now, still have to manually do cache-busting.
-# Put the current unix timestamp before .css and .js in the dist files, and update the references in index.html.
 dist:
-	rm -rf dist \
-		&& spago bundle-app --to dist/app.js \
-		&& sass src/stylesheets:dist --no-source-map \
-		&& node_modules/.bin/postcss web/app.css --use autoprefixer -o dist/app.css --no-map \
-		&& cp web/index.html dist \
-		&& node_modules/.bin/babel-minify dist/app.js -o dist/app.js --mangle --simplify --booleans --deadcode --builtIns --flipComparisons --memberExpressions --mergeVars --propertyLiterals
+	tools/dist.sh
---
+++ tools/dist.sh
@@ -0,0 +1,47 @@
+# run from the root dir
+
+# clear dist dir ===============================================================
+
+rm -rf dist
+
+# build dist/app.js ============================================================
+
+spago bundle-app --to dist/app.js
+
+# build dist/app.css ===========================================================
+
+sass src/stylesheets:dist --no-source-map
+
+# post-process dist/app.css ====================================================
+
+node_modules/.bin/postcss dist/app.css --use autoprefixer -o dist/app.css --no-map
+
+# copy index.html to dist ======================================================
+
+cp web/index.html dist
+
+# minifiy dist/index.html ======================================================
+
+node_modules/.bin/html-minifier dist/index.html --collapse-whitespace -o dist/index.html
+
+# minify dist/app.js ===========================================================
+
+node_modules/.bin/babel-minify dist/app.js -o dist/app.js --mangle --simplify --booleans --deadcode --builtIns --flipComparisons --memberExpressions --mergeVars --propertyLiterals
+
+# minify dist/app.css ==========================================================
+
+node_modules/.bin/cssnano dist/app.css dist/app.css
+
+# do cache busting =============================================================
+
+cache_bust()
+{
+    FILENAME=$1
+    EXT=$2
+    HASH=($(md5sum dist/$FILENAME.$EXT))
+    FILENAME_WITH_HASH=$FILENAME.$HASH.$EXT
+    mv dist/$FILENAME.$EXT dist/$FILENAME_WITH_HASH
+    sed -i "s/$FILENAME.$EXT/$FILENAME_WITH_HASH/g" dist/index.html
+}
+cache_bust app css
+cache_bust app js
